4783902122302bc52b7a5399f47ba548f356a5e9
JMarcan <jan.marcan@protonmail.com>
2020-04-05 12:22:55 +0200

    style: remove spaces from test case folder

--- Problem 2 - File Recursion.py
+++ Problem 2 - File Recursion.py
@@ -35,16 +35,26 @@ def find_files(suffix, path):
     return files_list
 
 def test_cases(path):
+    """
+    Execute test cases for function find_files(suffix, path) in a provided path
+    Print statements whether the test case passed or not are printed
+    
+    Args:
+      path(str): path of the folder in file system
+
+    Returns:
+       None
+    """
     
     # TC 1 - find .c files
-    expected_c_files = ['Problem 2  - testdir/subdir1/a.c', 'Problem 2  - testdir/subdir3/subsubdir1/b.c', 'Problem 2  - testdir/subdir5/a.c', 'Problem 2  - testdir/t1.c']
+    expected_c_files = ['Problem_2_testdir/subdir1/a.c', 'Problem_2_testdir/subdir3/subsubdir1/b.c', 'Problem_2_testdir/subdir5/a.c', 'Problem_2_testdir/t1.c']
     if find_files(".c", path) == expected_c_files: # returns 1
         print ("TC 1. Passed")
     else: 
         print ("TC 1. Failed")
     
     # TC 2 - find .h files
-    expected_h_files = ['Problem 2  - testdir/subdir1/a.h', 'Problem 2  - testdir/subdir3/subsubdir1/b.h', 'Problem 2  - testdir/subdir5/a.h', 'Problem 2  - testdir/t1.h']
+    expected_h_files = ['Problem_2_testdir/subdir1/a.h', 'Problem_2_testdir/subdir3/subsubdir1/b.h', 'Problem_2_testdir/subdir5/a.h', 'Problem_2_testdir/t1.h']
     if find_files(".h", path) == expected_h_files: # returns 1
         print ("TC 2. Passed")
     else: 
@@ -67,6 +77,6 @@ def test_cases(path):
 
 if __name__ == '__main__':
     # execute test cases  
-    path = "Problem 2  - testdir/"
+    path = "Problem_2_testdir/"
     test_cases(path)    
 
